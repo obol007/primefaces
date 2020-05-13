d7e9d459bfeee3fe81f6bcb4366f277b6f6cde60
obol007 <obol007@gmail.com>
2020-05-13 20:01:35 +0200

    hello jsf

---
+++ src/main/java/pl/coderslab/interview/controller/HelloController.java
@@ -0,0 +1,20 @@
+package pl.coderslab.interview.controller;
+
+import java.io.Serializable;
+
+public class HelloController implements Serializable {
+    public String msg="";
+
+    public String sayHello() {
+        if (msg.isEmpty()){
+            return "Hello World via JSF";
+        }
+        return "Hello " +  msg + " via JSF";
+    }
+
+
+    public void setMsg(String value){
+        this.msg = value;
+    }
+}
+
